000365a042104fa506c36d3910aa647b8082f21c
Albina <yakunina.albina@gmail.com>
2026-01-05 16:06:44 +0100

    Add new Header components: BurgerMenu, Buttons, Menu

---
+++ src/components/Header/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import { BurgerMenu } from '../BurgerMenu';
+import { Buttons } from '../Buttons';
+
+export const Menu = () => {
+  return (
+    <div>
+      <BurgerMenu />
+      <Buttons />
+    </div>
+  );
+};
