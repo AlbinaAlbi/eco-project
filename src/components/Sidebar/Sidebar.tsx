b065ef167c3f1ec3cd857acb116f9e6d676c2666
Albina <yakunina.albina@gmail.com>
2026-01-07 12:45:41 +0100

    Update Sidebar layout and styles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { useSidebar } from '../../context/SidebarContext';
-import { Explore } from '../Header/Menu/Explore';
 import { Language } from '../Header/Menu/Language';
+import { Volunteer } from '../Header/Menu/Volunteer';
 import { NavLinks } from '../Header/NavLinks';
 import styles from './Sidebar.module.scss';
 
@@ -10,9 +10,11 @@ export const Sidebar = () => {
   return (
     <div>
       <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
-        <NavLinks />
-        <Explore />
-        <Language />
+        <div className={styles.container}>
+          <NavLinks />
+          <Volunteer />
+          <Language />
+        </div>
       </div>
     </div>
   );
