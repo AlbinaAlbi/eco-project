0ac740b303fb27e2ecafcb5218f3fee07309f83f
Albina <yakunina.albina@gmail.com>
2026-01-07 15:03:22 +0100

    Update Menu component to work with new Sidebar context

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -1,16 +1,19 @@
 import styles from './Menu.module.scss';
 import { BurgerMenu } from './BurgerMenu';
 import { Language } from './Language';
-import { Volunteer } from './Volunteer';
+import { Button } from '../../Button';
+import { useLanguage } from '../../../context/LanguageContext';
 
 export const Menu = () => {
+  const { t } = useLanguage();
+
   return (
     <div className={styles.container}>
       <div className="desktopOnly">
         <Language />
       </div>
       <div className="tabletAndMore">
-        <Volunteer />
+        <Button text={t('volunteer')} />
       </div>
       <BurgerMenu />
     </div>
