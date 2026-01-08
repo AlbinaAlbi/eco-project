257987cf85b30c126f364f65bb2e79fd3f061dfd
Albina <yakunina.albina@gmail.com>
2026-01-08 11:56:42 +0100

    Add SECTION_HEADERS constants for header texts and colors

---
+++ src/locales/sectionHeaders.ts
@@ -0,0 +1,40 @@
+import { COLORS } from '../styles/colors';
+
+export const SECTION_HEADERS = {
+  action: {
+    tag: 'Take action',
+    title: 'Together, we createa greener future.',
+    tagColor: COLORS.gray100,
+    titleColor: COLORS.black,
+  },
+  mission: {
+    tag: 'Our mission',
+    title: 'We believe small actions create big change.',
+    tagColor: COLORS.gray100,
+    titleColor: COLORS.black,
+  },
+  works: {
+    tag: 'How it works',
+    title: 'Three simple steps to help you take meaningful action for nature',
+    tagColor: COLORS.gray200,
+    titleColor: COLORS.black,
+  },
+  featured: {
+    tag: 'Featured projects',
+    title: 'Explore initiatives that matter',
+    tagColor: COLORS.gray100,
+    titleColor: COLORS.black,
+  },
+  faq: {
+    tag: 'FAQ',
+    title: 'Common questions about our work',
+    tagColor: COLORS.gray200,
+    titleColor: COLORS.black,
+  },
+  start: {
+    tag: 'Start your initiative',
+    title: 'Have an idea for your own eco-project',
+    tagColor: COLORS.gray100,
+    titleColor: COLORS.white,
+  },
+};
