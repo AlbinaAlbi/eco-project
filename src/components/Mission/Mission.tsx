2784ad4be4550b6e817f16dcdc8f53e4f830f1fd
Albina <yakunina.albina@gmail.com>
2026-01-12 13:39:27 +0100

    Implement Mission component functionality

--- src/components/Mission/Mission.tsx
+++ src/components/Mission/Mission.tsx
@@ -3,7 +3,9 @@ import { SECTION_HEADERS } from '../../locales/sectionHeaders';
 import { Image } from '../Image';
 import { TagAndTitle } from '../TagAndTitle';
 import styles from './Mission.module.scss';
-import TakeActionImg from '../../imgs/Become a Volunteer.png';
+import MissionImg1 from '../../imgs/mission-card-1.jpg';
+import { CardLearnMore } from './CardLearnMore';
+import { ConnectPeopleCard } from './ConnectPeopleCard';
 
 export const Mission = () => {
   const { tagKey, titleKey, tagColor, titleColor } = SECTION_HEADERS.mission;
@@ -16,8 +18,12 @@ export const Mission = () => {
     <div className={styles.container}>
       <TagAndTitle tag={tag} title={title} tagColor={tagColor} titleColor={titleColor} />
 
-      <div className={styles.img}>
-        <Image img={TakeActionImg} alt={'Become a Volunteer'} />
+      <div className={styles.cards}>
+        <Image img={MissionImg1} alt={'Mission card 1'} />
+        <div className={styles.imgContainer}>
+          <ConnectPeopleCard />
+          <CardLearnMore />
+        </div>
       </div>
     </div>
   );
