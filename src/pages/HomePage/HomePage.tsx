c9c93fe8a5cf6317d6de896d293e58aa94b72a90
Albina <yakunina.albina@gmail.com>
2026-01-13 10:59:05 +0100

    Add FeaturedProjects section to HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,20 @@
+import { FeaturedProjects } from '../../components/FeaturedProjects';
+import { HowItWorks } from '../../components/HowItWorks';
 import { Mission } from '../../components/Mission';
 import { TakeAction } from '../../components/TakeAction';
 import styles from './HomePage.module.scss';
 
 export const HomePage = () => {
   return (
-    <div className={`containerMaxWidth ${styles.container}`}>
-      <TakeAction />
-      <Mission />
+    <div className={`${styles.container}`}>
+      <div className={`containerMaxWidth containerContentPadding ${styles.container}`}>
+        <TakeAction />
+        <Mission />
+      </div>
+      <HowItWorks />
+      <div className={`containerMaxWidth containerContentPadding ${styles.container}`}>
+        <FeaturedProjects />
+      </div>
     </div>
   );
 };
