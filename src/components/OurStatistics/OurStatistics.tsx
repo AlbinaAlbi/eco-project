4ea9c99e1bb014d54a0496547ee3d15149e9b4e3
Albina <yakunina.albina@gmail.com>
2026-01-14 12:06:17 +0100

    Display projects count in statistics section

--- src/components/OurStatistics/OurStatistics.tsx
+++ src/components/OurStatistics/OurStatistics.tsx
@@ -1,16 +1,48 @@
+import { useLanguage } from '../../context/LanguageContext';
 import { useSectionHeader } from '../../hooks/useSectionHeader';
 import { SECTION_HEADERS } from '../../locales/sectionHeaders';
 import { Description } from '../Description';
 import { TagAndTitle } from '../TagAndTitle';
 import styles from './OurStatistics.module.scss';
 
-export const OurStatistics = () => {
+interface OurStatisticsProps {
+  projectsLength: number;
+}
+export const OurStatistics = ({ projectsLength }: OurStatisticsProps) => {
   const { tagKey, titleKey, tagColor, descriptionKey, titleColor } = SECTION_HEADERS.statistics;
   const { tag, title, description } = useSectionHeader({
     tagKey,
     titleKey,
     descriptionKey,
   });
+  const { t } = useLanguage();
+
+  const statisticsList = [
+    {
+      id: 1,
+      titleKey: t('statisticsList.statistic1Title'),
+      descriptionKey: t('statisticsList.statistic1Description'),
+      count: t('statisticsList.statistic1Count'),
+    },
+    {
+      id: 2,
+      titleKey: t('statisticsList.statistic2Title'),
+      descriptionKey: t('statisticsList.statistic2Description'),
+      count: t('statisticsList.statistic2Count'),
+    },
+    {
+      id: 3,
+      titleKey: t('statisticsList.statistic2Title'),
+      descriptionKey: t('statisticsList.statistic2Description'),
+      count: `${projectsLength}`,
+    },
+    {
+      id: 4,
+      titleKey: t('statisticsList.statistic2Title'),
+      descriptionKey: t('statisticsList.statistic2Description'),
+      count: t('statisticsList.statistic2Count'),
+    },
+  ];
 
   return (
     <div className={styles.container}>
