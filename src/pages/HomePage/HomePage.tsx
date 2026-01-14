89a5a6182b254eeb57516023923358363fa48a30
Albina <yakunina.albina@gmail.com>
2026-01-14 11:30:15 +0100

    Update Home page layout and sections

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
 import { FeaturedProjects } from '../../components/FeaturedProjects';
 import { HowItWorks } from '../../components/HowItWorks';
 import { Mission } from '../../components/Mission';
+import { OurStatistics } from '../../components/OurStatistics';
 import { TakeAction } from '../../components/TakeAction';
 import styles from './HomePage.module.scss';
 
 export const HomePage = () => {
   return (
-    <div className={`${styles.container}`}>
+    <div className={styles.container}>
       <div className={`containerMaxWidth containerContentPadding ${styles.container}`}>
         <TakeAction />
         <Mission />
@@ -15,6 +16,7 @@ export const HomePage = () => {
       <div className={`containerMaxWidth containerContentPadding ${styles.container}`}>
         <FeaturedProjects />
       </div>
+      <OurStatistics />
     </div>
   );
 };
