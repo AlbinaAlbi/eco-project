d788a37ad7486b045d08e46fc3e8e9b1af0b898d
Albina <yakunina.albina@gmail.com>
2026-01-16 09:34:17 +0100

    Update ProjectsPage component

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,32 +1,12 @@
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import { fetchProjects } from '../../api/projects';
-import { setProjects } from '../../store/slices/ProjectsSlice/projectsSlice';
+import { ExploreProjects } from '../../components/ExploreProjects';
+import { useAppSelector } from '../../hooks/hooks';
 
 export const ProjectsPage = () => {
-  const dispatch = useAppDispatch();
-  const projects = useAppSelector((state) => state.projects.projects);
+  const { projects } = useAppSelector((state) => state.projects);
 
-  useEffect(() => {
-    const loadProjects = async () => {
-      const data = await fetchProjects();
-      const dataWithStringId = data.map((p) => ({ ...p, id: p.id.toString() }));
-
-      dispatch(setProjects(dataWithStringId));
-    };
-
-    loadProjects();
-  }, [dispatch]);
-
-  console.log(projects);
   return (
     <div>
-      <h1>Projects</h1>
-      <ul>
-        {projects.map((p) => (
-          <li key={p.id}>{p.title}</li>
-        ))}
-      </ul>
+      <ExploreProjects />
     </div>
   );
 };
