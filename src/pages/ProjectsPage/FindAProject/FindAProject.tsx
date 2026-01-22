09455bf75583d194edf9e5f95bf51cd6a3bcb172
Albina <yakunina.albina@gmail.com>
2026-01-22 16:55:10 +0100

    fix button text

--- src/pages/ProjectsPage/FindAProject/FindAProject.tsx
+++ src/pages/ProjectsPage/FindAProject/FindAProject.tsx
@@ -1,6 +1,7 @@
 import { Button } from '../../../components/Button';
 import { Description } from '../../../components/Description';
 import { TagAndTitle } from '../../../components/TagAndTitle';
+import { useLanguage } from '../../../context/LanguageContext';
 import { useDeviceType } from '../../../hooks/getDeviceType';
 import { useSectionHeader } from '../../../hooks/useSectionHeader';
 import { SECTION_HEADERS } from '../../../locales/sectionHeaders';
@@ -13,14 +14,13 @@ export const FindAProject = () => {
     descriptionKey,
   });
   const device = useDeviceType();
+  const { t } = useLanguage();
 
   return (
     <div className={`containerContentPadding ${styles.container}`}>
       <TagAndTitle title={title} titleColor={titleColor} />
       <Description title={description} />
-      {device !== 'mobile' && (
-        <Button text={'Submit a project'} color={'green'} buttonWidth={'320px'} />
-      )}
+      {device !== 'mobile' && <Button text={t('submit')} color={'green'} buttonWidth={'320px'} />}
     </div>
   );
 };
