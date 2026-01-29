5ddb638c702b6392178cedaa85b01dd809a04e7a
Albina <yakunina.albina@gmail.com>
2026-01-29 19:29:09 +0100

    add Image element for HeaderProject

--- src/pages/ProjectDetailPage/HeaderProject/HeaderProject.tsx
+++ src/pages/ProjectDetailPage/HeaderProject/HeaderProject.tsx
@@ -2,6 +2,7 @@ import { Button } from '../../../components/Button';
 import { Description } from '../../../components/Description';
 import { ProgressBar } from '../../../components/FeaturedProjects/ProjectCard/ProgressBar';
 import { Region } from '../../../components/FeaturedProjects/ProjectCard/Region';
+import { Image } from '../../../components/Image';
 import { Status } from '../../../components/Status';
 import { TagAndTitle } from '../../../components/TagAndTitle';
 import { VolunteersNeeded } from '../../../components/VolunteersNeeded';
@@ -12,6 +13,7 @@ import { useTranslatedCity } from '../../../hooks/useTranslatedCity';
 import { Project } from '../../../types/Project';
 import { ProjectDetail } from '../../../types/ProjectDetail';
 import styles from './HeaderProject.module.scss';
+import man from '../../../imgs/man plantin.png';
 
 interface HeaderProjectProps {
   project: Project;
@@ -57,14 +59,18 @@ export const HeaderProject = ({ project, projectDetail }: HeaderProjectProps) =>
         <Region regionText={cityTranslations} />
         <Status status={project.status} />
         <VolunteersNeeded count={39} />
-        <div className={styles.progressBar}>
-          <ProgressBar goalAmount={project.goalAmount} currentAmount={project.currentAmount} />
+      </div>
+      <div className={styles.progressBar}>
+        <ProgressBar goalAmount={project.goalAmount} currentAmount={project.currentAmount} />
 
-          <div className={styles.buttons}>
-            <Button text={t('explore')} buttonWidth={buttonWidth} />
-            <Button text={t('volunteer')} color={'white'} buttonWidth={buttonWidth} />
-          </div>
+        <div className={styles.buttons}>
+          <Button text={t('explore')} buttonWidth={buttonWidth} />
+          <Button text={t('volunteer')} color={'white'} buttonWidth={buttonWidth} />
         </div>
+      </div>
+
+      <div className={styles.image}>
+        <Image img={man} alt={'Man plantin'} />
       </div>
     </div>
   );
